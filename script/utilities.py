7e5b21a05a7cd99f97c1a38bb05d16c07213a3e7
Sean Soutar <s1959963@ed.ac.uk>
2020-07-08 17:05:15 +0100

    negative matches similarity returns indices properly

--- script/utilities.py
+++ script/utilities.py
@@ -23,9 +23,6 @@ g_train = g_train.rename(columns = {"price":"price_g","id":"id_g",'name':'title_
 ## Join Amazon Products
 # perfect_matches = pd.merge(amz_train, matches_train, how = 'inner', left_index=  True , right_on = matches_train.index.get_level_values("idAmazon"), suffixes = ("amzn_x","amzn_y"))
 # perfect_matches = perfect_matches.iloc[:,2:5]
-
-
-
 # ## Join Google Products
 # perfect_matches = pd.merge(g_train, perfect_matches, how = 'inner', left_index=  True , right_on = perfect_matches.index.get_level_values("idGoogleBase"), suffixes = ("g_x","g_y"))
 # perfect_matches = perfect_matches.rename(columns = {'name':'title_g', "description":"description_g","manufacturer":"manufacturer_g"})
@@ -42,7 +39,7 @@ g_train = g_train.rename(columns = {"price":"price_g","id":"id_g",'name':'title_
 def calculate_edit_distance(x,cols):
     return fuzz.ratio("".join(str(x[cols[0]])), "".join(str(x[cols[1]])))
 
-def generate_distance_samples(n, true_matches, negative_matches, distance_cols, plot = True, return_sim = False):
+def generate_distance_samples(n, true_matches, negative_matches, id_names, distance_cols, plot = True, return_sim = False):
     '''
     Inputs:
         n: Sample Size of true and positive match pairs to take WITHOUT replacement
@@ -62,14 +59,16 @@ def generate_distance_samples(n, true_matches, negative_matches, distance_cols, 
     if (n > negative_matches[0].shape[0] or n > negative_matches[1].shape[0] or n > true_matches.shape[0]):
         raise ValueError('Sample size n is greater in length than at least one of the tables.')
 
-    negative_matches[0] = negative_matches[0].sample(n = n).reset_index(drop = True)
-    negative_matches[1] = negative_matches[1].sample(n = n).reset_index(drop = True)
+    negative_matches[0] = negative_matches[0].sample(n = n).reset_index(drop = False)
+    negative_matches[1] = negative_matches[1].sample(n = n).reset_index(drop = False)
     negative_matches_sample = pd.concat(negative_matches, axis = 1)
+    negative_matches_sample = negative_matches_sample.set_index(id_names)
 
     true_matches_sample = true_matches.sample(n = n)
     
-    true_matches_sample["similarity"]= true_matches_sample.apply(axis = 1, func = calculate_edit_distance, cols = distance_cols)
-    negative_matches_sample["similarity"]= negative_matches_sample.apply(axis = 1, func = calculate_edit_distance, cols = distance_cols)
+    # Calculate pairwise similarities across each row
+    true_matches_sample["similarity"] = true_matches_sample.apply(axis = 1, func = calculate_edit_distance, cols = distance_cols)
+    negative_matches_sample["similarity"] = negative_matches_sample.apply(axis = 1, func = calculate_edit_distance, cols = distance_cols)
 
     if plot:
         sns.distplot(true_matches_sample.similarity)
@@ -77,6 +76,12 @@ def generate_distance_samples(n, true_matches, negative_matches, distance_cols, 
     if return_sim:
         return (true_matches_sample.similarity, negative_matches_sample.similarity)
 
+# # DEBUG distance function
+# true_matches = generated_matches[0]
+# negative_matches = generated_matches[1]
+# distance_cols = [["title_g","description_g"],["title_amzn","description_amzn"]]
+# n = 100
+# id_names = ["id_amzn","id_g"]
 
 def generate_pos_neg_matches(positive_matching_table, table_list, id_names, feature_cols, table_names = None):
     '''
@@ -92,7 +97,6 @@ def generate_pos_neg_matches(positive_matching_table, table_list, id_names, feat
     Outputs:
             (positive_matches, negative_matches)
     '''
-
     # Make Sure Nested Indexing is present. First poisition of Id names is the Id of the left table
     positive_matching_table.set_index(keys = id_names, inplace = True)
     
@@ -121,25 +125,68 @@ def generate_pos_neg_matches(positive_matching_table, table_list, id_names, feat
 
     return (positive_matches, negative_matches_list)
 
-def generate_em_train_valid_split(generated_matches, rules, prop_train = 0.8):
-
-    '''
-    Inputs:
-            generated_matches: output of generate_pos_neg_matches()
-            rules: a list of length 2 denoting the cutoff PERCENTILES for easy, medium and hard training examples
-            by distance. For example [0.05,0.95] means that closest 5% of distances are the easy examples, middle
-            90% are the normal and top 5% are the difficult
-
-    '''
-    raise NotImplementedError
-
-
+# # Debug generate pos neg matches
 # table_list = [amz_train, g_train]
 # id_names = ["id_amzn","id_g"]
 # feature_cols = ['title_amzn', 'description_amzn',
 #        'manufacturer_amzn', 'price_amzn', 'title_g', 'description_g', 'manufacturer_g',
 #        'price_g']
 # positive_matching_table = matches_train
+
+def generate_em_train_valid_split(generated_matches, id_names, difficult_cutoff = 0.1, prop_train = 0.8):
+
+    '''
+    Inputs:
+            generated_matches: output of generate_pos_neg_matches()
+
+            difficult_cutoff: 
+
+            prop_train: proportion of data allocated to training DataFrame
+    Outputs: 
+            (X_train, y_train, X_valid, y_valid)
+
+    '''
+
+    # TODO: Need to think about how the final data size will be calculated
+    #       Perhaps have a smaller bigger table index?
+    total_size = generated_matches[0].shape[0] + generated_matches[1][0].shape[0] + generated_matches[1][1].shape[0]
+    train_size = round(total_size * prop_train,0)
+    valid_size = total_size - train_size
+
+    # First, generate a cutoff rule in terms of similarity measure units according to difficult_cutoff
+    pos_sim, neg_sim = generate_distance_samples(100, generated_matches[0], generated_matches[1], id_names, [["title_g","description_g"],["title_amzn","description_amzn"]], False, True)
+    # Lowest similarity is more difficult for positive matches and higher similarity
+    # is more difficult for negative matches
+    pos_sim_cutoff = np.quantile(pos_sim, difficult_cutoff)
+    neg_sim_cutoff = np.quantile(neg_sim, 1-difficult_cutoff)
+
+    # Iteratively Sample until we have the desired proportion of difficult negative and positive matches
+    positive_matches = generated_matches[0]
+    negative_matches = generated_matches[1]
+
+    pos_difficult_indices = []
+    neg_difficult_indices = []
+
+
+    for _ in np.arange(10):
+        pos_indices, neg_indices = generate_distance_samples(100, positive_matches, negative_matches, id_names, [["title_g","description_g"],["title_amzn","description_amzn"]], False, True)
+        # Add indices to to *_difficult_indices iff they are beyond cutoff rules
+        # therefore they are classified as being difficult examples
+        pos_difficult_indices.append(pos_indices[pos_indices <= pos_sim_cutoff].index)
+        neg_difficult_indices.append(neg_indices[neg_indices >= neg_sim_cutoff].index)
+        # Remove these stored indices from the tables to avoid double sampling
+        removed_pos_bool = positive_matches.index.isin(pos_indices[pos_indices <= pos_sim_cutoff].index)
+        removed_neg_bool = negative_matches.index.isin(neg_indices[neg_indices >= neg_sim_cutoff].index)
+        # Check if target density of difficuly examples have been achieved, if it has simply randomly sample the rest and don't calc distances 
+
+
+    train_indices = generated_matches[0]
+
+# DEbug generate train valid
+generated_matches = generated_matches
+id_names = ["id_amzn","id_g"]
+prop_train = 0.8
+difficult_cutoff = 0.9
 
 generated_matches = generate_pos_neg_matches(matches_train,
                                             [amz_train, g_train], 
@@ -148,4 +195,5 @@ generated_matches = generate_pos_neg_matches(matches_train,
        'manufacturer_amzn', 'price_amzn', 'title_g', 'description_g', 'manufacturer_g',
        'price_g'])
 
-generate_distance_samples(200, generated_matches[0], generated_matches[1], [["title_g","description_g"],["title_amzn","description_amzn"]])
+generate_distance_samples(200, generated_matches[0], generated_matches[1], ["id_amzn","id_g"],[["title_g","description_g"],["title_amzn","description_amzn"]])
+
