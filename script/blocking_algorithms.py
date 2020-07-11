927274dbaf5f9178d38f19a104f45d07de9639ed
Sean Soutar <s1959963@ed.ac.uk>
2020-07-11 18:53:30 +0100

    overlapped_att_blocking working

--- script/blocking_algorithms.py
+++ script/blocking_algorithms.py
@@ -7,6 +7,13 @@ import py_entitymatching as em
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from utilities import partition_data_set
 
+#https://onestopdataanalysis.com/lsh/
+# https://anhaidgroup.github.io/py_entitymatching/v0.3.2/singlepage.html
+#https://sites.google.com/site/anhaidgroup/projects/magellan/issues
+
+
+
+
 def overlapped_attribute_blocking(data_set, blocking_cols, min_shared_tokens, feature_cols, id_names, verbose = True):
     '''
     Overlapp Blocking Algorithm
@@ -20,29 +27,15 @@ def overlapped_attribute_blocking(data_set, blocking_cols, min_shared_tokens, fe
         Candidate Tuples
 
     '''
+    # Clear Catalog of previous entries
+    em.del_catalog()
     overlap = em.OverlapBlocker()
-   
-    data_set = em.read_csv_metadata("../data/processed_amazon_google/amz_google_X_train.csv").reset_index()
-    em.set_key(data_set, "index")
-    blocking_cols = ["manufacturer_amzn","manufacturer_g"]
-    min_shared_tokens = 2
-    feature_cols  = [['title_amzn',
- 'description_amzn',
- 'manufacturer_amzn',
- 'price_amzn'],
- ['title_g',
- 'description_g',
- 'manufacturer_g',
- 'price_g']]
- verbose = True
-
-    id_names =amz_goog.data.id_names 
-
-
-    lhs_table, rhs_table = partition_data_set(data_set, id_names, feature_cols[0] + feature_cols[1])
-    # Need to set key id as per the catalog functionality
-    em.set_key(lhs_table.reset_index(), "index")
-    em.set_key(rhs_table.reset_index(), "index")
+    # Read in Data into memory
+    lhs_table = em.read_csv_metadata("../data/processed_amazon_google/amz_google_X_train_lhs.csv").rename(columns = {"Unnamed: 0":"id_lhs"})
+    rhs_table = em.read_csv_metadata("../data/processed_amazon_google/amz_google_X_train_rhs.csv").rename(columns = {"Unnamed: 0":"id_rhs"})
+    em.del_catalog()
+    em.set_key(lhs_table, "id_lhs")
+    em.set_key(rhs_table, "id_rhs")
 
     candidate_pairs = overlap.block_tables(lhs_table, rhs_table,
                     blocking_cols[0], blocking_cols[1], 
@@ -55,14 +48,20 @@ def overlapped_attribute_blocking(data_set, blocking_cols, min_shared_tokens, fe
 
 
 
-# overlapped_attribute_blocking(amz_goog.data.train_valid_sets[0], ["manufacturer_amzn","manufacturer_g"], 2, [['title_amzn',
-#  'description_amzn',
-#  'manufacturer_amzn',
-#  'price_amzn'],
-#  ['title_g',
-#  'description_g',
-#  'manufacturer_g',
-#  'price_g']], amz_goog.data.id_names)
+overlapped_attribute_blocking()
+
+blocking_cols = ["manufacturer_amzn","manufacturer_g"]
+min_shared_tokens = 3
+feature_cols  = [['title_amzn',
+'description_amzn',
+'manufacturer_amzn',
+'price_amzn'],
+['title_g',
+'description_g',
+'manufacturer_g',
+'price_g']]
+verbose = True
+
 
 
 def locality_sensitive_hashing_blocking():
@@ -73,8 +72,3 @@ def locality_sensitive_hashing_blocking():
 
 
 
-#https://onestopdataanalysis.com/lsh/
-# https://anhaidgroup.github.io/py_entitymatching/v0.3.2/singlepage.html
-#https://sites.google.com/site/anhaidgroup/projects/magellan/issues
-import py_entitymatching as em
-em.OverlapBlocker()
