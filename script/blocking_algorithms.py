5f30bed6e65a10aac8d76763a85900c38c2f74f7
Sean Soutar <s1959963@ed.ac.uk>
2020-07-11 01:26:22 +0100

    think Magellan implementation blocking techniques

--- script/blocking_algorithms.py
+++ script/blocking_algorithms.py
@@ -1,2 +1,22 @@
 # This script includes all blocking algorithm implementation
 
+
+def shared_attributes_blocking(distance_cols):
+    '''
+    Blocking Algorithm which collects candidates with respect to sharing some selected attributes
+    '''
+    raise NotImplementedError
+
+def locality_sensitive_hashing_blocking():
+
+    raise NotImplementedError
+
+
+
+
+
+#https://onestopdataanalysis.com/lsh/
+# https://anhaidgroup.github.io/py_entitymatching/v0.3.2/singlepage.html
+#https://sites.google.com/site/anhaidgroup/projects/magellan/issues
+import py_entitymatching as em
+em.OverlapBlocker()
