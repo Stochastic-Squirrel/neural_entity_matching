746328d78430caf9ffc6db49947d715f9ff6164e
Sean Soutar <s1959963@ed.ac.uk>
2020-07-17 21:40:17 +0100

    general testing framework for deepmatcher WIP

--- script/model_magellan_ml.py
+++ script/model_magellan_ml.py
@@ -1,6 +1,22 @@
 '''
 Entity matching model using Automatic Feature creation of magellan and tests a host
 of algorithms.
+
+Magellan Model all_result dictionary hierarchy
+
+    Sampler; Blocking Algorithm; Result object
+
+    For a given Sampler and Blocking algo:
+        Result Object[experiment_id corresponding to Sampler-Blocking iteration][Set ID][Model Name when appropriate]
+        Set Ids:
+            0: Training Predictions
+            1: Validation Predictions
+            2: Test Set Predictions
+            3: All Sets pre-blocked labels
+            4: All sets post-blocked labels
+            5: Experiment Meta Data
+
+
 '''
 import os
 import py_entitymatching as em
